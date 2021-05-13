db3e419213c41913cbb3a8db110fdaab802ac12d
Domtron Vox <domtron.vox@gmail.com>
2021-05-13 01:27:22 +0000

    Added a common function to help manipulate template files.

--- scripts/common/functions.sh
+++ scripts/common/functions.sh
@@ -2,6 +2,11 @@
 
 
 
+
+###########
+# Functions that check on user privlages
+###########
+
 #Are we running as the root user
 check_root () {
   if [[ $EUID -ne 0 ]]; then
@@ -9,3 +14,29 @@ check_root () {
     return 1
   fi
 }
+
+
+
+
+###########
+# Functions for Template Manipulation
+###########
+
+#Define the open and close delimiters for tags
+OPEN_TEMPLATE_TAG="<<"
+CLOSE_TEMPLATE_TAG=">>"
+
+
+# Replaces all intances of a given tag name in a given file with the given data
+## Needs 3 argumenst. 1st is the filepath of the file to replace in,
+##                    2nd is the name of the tag to replace
+##                    3rd is the data to replace it with
+replace_tag() {
+    local filepath="$1"
+    local tag="${OPEN_TEMPLATE_TAG}${2}${CLOSE_TEMPLATE_TAG}"
+    local replace_text="$3"
+
+    #Uses a global substitution regex to replace all instances of tag with replace_text
+    sed --in-place "s+${tag}+${replace_text}+g" "$replace_text"
+}
+
