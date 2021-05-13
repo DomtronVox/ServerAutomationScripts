d18939111d2399d48e044aa152eb6c4a4bdc0df9
Domtron Vox <domtron.vox@gmail.com>
2021-05-13 17:48:43 +0000

    Added a task to install_nginx that sets up let's encrypt for nginx.

--- scripts/nginx/install_nginx.sh
+++ scripts/nginx/install_nginx.sh
@@ -38,7 +38,7 @@ install_nginx() {
     # Do task
     ui_task_note "Performing Task."
 
-    sudo apt install nginx
+    sudo apt install -y nginx
 
     # Check for errors
     ui_task_note "Checking for errors."
@@ -63,8 +63,49 @@ fi
 ui_task_end
 
 
-#Task 2
-#...
+#Task: setup lets encrypt for nginx
+ui_task_start "Setup Let's Encrypt for NGINX"
+perform_task=$?
+
+setup_letsencrypt() {
+    # Check if safe and needed
+    ui_task_note "Checking if task is safe to run and if it is needed."
+
+    if which certbot > /dev/null 2>&1 && [[ -e /etc/letsencrypt/options-ssl-nginx.conf ]]; then
+        log_warn "Both certbot and the nginx plugin are installed so it is not needed."
+	return 1
+    fi
+
+    # Do task
+    ui_task_note "Performing Task."
+    
+    log_msg "Install certbot and the NGINX certbot plugin..."
+    sudo apt install --assume-yes certbot python3-certbot-nginx
+
+    log_msg "Tell certbot to create the nginx specific config files we will use in nginx config files..."
+    # c is for cancel since we don't want to enter any domains for this
+    echo "c" | sudo certbot --nginx --register-unsafely-without-email --agree-tos
+    
+    # Check for errors
+    ui_task_note "Checking for errors."
+
+    if ! which certbot > /dev/null 2>&1 || [[ ! -e /etc/letsencrypt/options-ssl-nginx.conf ]]; then
+        log_err "Still missing certbot and/or the nginx plugin!"
+        return 1
+    fi
+
+
+}
+if [ "$perform_task" -eq 0 ]; then
+    setup_letsencrypt
+    installed_letsencrypt=$?
+else
+    log_warn "You chose not to run this task."
+    installed_letsencrypt=1
+fi
+
+ui_task_end
+
 
 
 # Summery
@@ -73,15 +114,20 @@ ui_section_summery_start "Script $DOC_NAME"
 if [ "$installed_nginx" == 0 ]; then
     ui_task_note "Installed nginx."
 else
-    log_warn "Could not install nginx read output for errors why."
+    log_warn "Could not install nginx read output for errors or warnings why."
 fi
 
+if [ "$installed_letsencrypt" == 0 ]; then
+    ui_task_note "Installed letsencrypt and plugin for nginx."
+else
+    log_warn "Could not install letsencrypt for nginx read output for errors why."
+fi
 
 log_msg "$(cat <<- HEREDOC
 	 
 	>Further Instructions<
 	Assuming no errors are listed above, NGINX is now ready to use. 
-	You will need to configure the server for what you need it for. However, a basic default page should be avalible on port 80 now.
+	You will need to configure the server for what you need it for. However, a basic default page should be avalible on port 80 right now.
 	 
 	HEREDOC
 )"
